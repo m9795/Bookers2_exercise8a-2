2a8f15d9f7de17861c92e2fc154d77bd2df4e03d
m9795 <106650955+m9795@users.noreply.github.com>
2022-06-28 19:02:00 +0900

    [Add]モデル作成とアソシエーションまで

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books
+  has_many :user_rooms
+  has_many :chats
+  has_many :rooms, through: :user_rooms
   
   # 自分がフォローされる（被フォロー）側の関係性
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
